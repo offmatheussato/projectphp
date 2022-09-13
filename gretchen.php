e673a1353bf3257910a6fd148d30ba703cd65419
Matheus Sato <101195883+offmatheussato@users.noreply.github.com>
2022-09-13 10:14:57 -0300

    Add files via upload
    Introduction in PHP program

---
+++ gretchen.php
@@ -0,0 +1,28 @@
+<?php
+    $op = 1;
+    switch ($op) 
+    {
+        case 1: 
+            print "1";
+            break;
+
+            case 2: 
+                print "2";
+                break;
+
+                case 3: 
+                    print "3";
+                    break;
+
+                    case 4: 
+                        print "4";
+                        break;
+
+                        default:
+                        print "op invalida";
+                        break;
+
+    }
+
+
+?>
---
+++ sato.php
@@ -0,0 +1,11 @@
+<?php
+	$n = 1;
+    if ($n==7)
+        print "zero";
+    else 
+    {
+    print"não zero";
+    print"<br> acabou o porgrama e foi no zero";    
+    }
+
+?>
